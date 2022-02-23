57ead601ca41146edb0452f13c348a5c650b29c9
2612705 <2612705w@student.gla.ac.uk>
2022-02-23 15:13:39 +0000

    chapter 9

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,7 @@ from turtle import title
 from unicodedata import category
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -25,7 +26,15 @@ class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default=0) 
     
     def __str__(self):
         return self.title
+    
+class UserProfile(models.Model):
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   website = models.URLField(blank=True)
+   picture = models.ImageField(upload_to='profile_images', blank=True)
+   
+   def __str__(self):
+       return self.user.username
